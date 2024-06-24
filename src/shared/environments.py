d79980561fccb1b27d7fcacd41325bf0c584e846
LucaPinheiro <lucapgomes11@gmail.com>
2024-06-24 02:55:03 -0300

    refactor: Update Environments class to include db_url property

--- src/shared/environments.py
+++ src/shared/environments.py
@@ -1,17 +1,15 @@
 import os
 from shared.domain.enums.stage_enum import STAGE
-
 from shared.domain.irepositories.user_repository_interface import IUserRepository
 from shared.infra.repositories.user_repository_postgres import UserRepositoryPostgres
 
 
 class Environments:
     """
-    Defines the environment variables for the application. You should not instantiate this class directly. Please use Environments.get_envs() method instead.
-
-    Usage:
-
+    Defines the environment variables for the application. You should not instantiate this class directly. 
+    Please use Environments.get_envs() method instead.
     """
+
     stage: STAGE
     s3_bucket_name: str
     region: str
@@ -19,8 +17,9 @@ class Environments:
     dynamo_table_name: str
     dynamo_partition_key: str
     dynamo_sort_key: str
-    cloud_frontget_user_presenter_distribution_domain: str
-    mss_name: str 
+    cloud_front_distribution_domain: str
+    mss_name: str
+    db_url: str
 
     def _configure_local(self):
         os.environ["STAGE"] = os.environ.get("STAGE") or STAGE.TEST.value
@@ -31,7 +30,8 @@ class Environments:
 
         self.stage = STAGE[os.environ.get("STAGE")]
         self.mss_name = os.environ.get("MSS_NAME")
-        
+        self.db_url = os.environ.get("SQLALCHEMY_DATABASE_URL")
+
         if self.stage == STAGE.TEST:
             self.s3_bucket_name = "bucket-test"
             self.region = "sa-east-1"
@@ -48,13 +48,15 @@ class Environments:
             self.dynamo_table_name = os.environ.get("DYNAMO_TABLE_NAME")
             self.dynamo_partition_key = os.environ.get("DYNAMO_PARTITION_KEY")
             self.dynamo_sort_key = os.environ.get("DYNAMO_SORT_KEY")
-            self.cloud_front_distribution_domain = os.environ.get("CLOUD_FRONT_DISTRIBUTION_DOMAIN")
+            self.cloud_front_distribution_domain = os.environ.get(
+                "CLOUD_FRONT_DISTRIBUTION_DOMAIN")
 
     @staticmethod
     def get_user_repository() -> IUserRepository:
-        if Environments.get_envs().stage == STAGE.TEST:
-            return UserRepositoryPostgres()
-        elif Environments.get_envs().stage in [STAGE.DEV, STAGE.HOMOLOG, STAGE.PROD]:
+        envs = Environments.get_envs()
+        if envs.stage == STAGE.TEST:
+            return UserRepositoryPostgres(envs.db_url)
+        elif envs.stage in [STAGE.DEV, STAGE.HOMOLOG, STAGE.PROD]:
             from shared.infra.repositories.user_repository_cognito import UserRepositoryCognito
             return UserRepositoryCognito()
         else:
@@ -65,12 +67,10 @@ class Environments:
         """
         Returns the Environments object. This method should be used to get the Environments object instead of instantiating it directly.
         :return: Environments (stage={self.stage}, s3_bucket_name={self.s3_bucket_name}, region={self.region}, endpoint_url={self.endpoint_url})
-
         """
         envs = Environments()
         envs.load_envs()
         return envs
 
     def __repr__(self):
-        return self.__dict__
-
+        return str(self.__dict__)
