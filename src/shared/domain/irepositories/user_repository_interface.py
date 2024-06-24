04ebf238027080773460def6770f7b9e3882b237
LucaPinheiro <lucapgomes11@gmail.com>
2024-06-23 23:24:04 -0300

    refactor: Add create_user method to IUserRepository interface

--- src/shared/domain/irepositories/user_repository_interface.py
+++ src/shared/domain/irepositories/user_repository_interface.py
@@ -8,3 +8,11 @@ class IUserRepository(ABC):
   @abstractmethod
   def get_all_users(self) -> List[User]:
     pass
+  
+  @abstractmethod
+  def create_user(self, name: str, email: str, phone: str, password: str) -> User:
+    pass
+  
+  @abstractmethod
+  def get_user_by_id(self, user_id: int) -> User:
+    pass
