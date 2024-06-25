ef04eb54c27d15a576d523321e243c39a123ee22
LucaPinheiro <lucapgomes11@gmail.com>
2024-06-24 22:44:01 -0300

    refactor: Improve login usecase error handling and parameter validation

--- src/modules/login/app/login_usecase.py
+++ src/modules/login/app/login_usecase.py
@@ -1,10 +1,23 @@
+from typing import Any
+from src.shared.domain.entities.user import User
 from src.shared.domain.irepositories.user_repository_interface import IUserRepository
+from src.shared.helpers.errors.domain_errors import EntityError
+from src.shared.helpers.errors.usecase_errors import InvalidCredentials
 
 
 class LoginUsecase:
     def __init__(self, repo: IUserRepository):
-        self.repo = repo
-
-    def execute(self, email, password):
-        login = self.repo.login(email, password)
-        return login
+        self.user_repository = repo
+        
+    def __call__(self, email: str, password: str) -> dict:
+        if not User.validate_email(email):
+            raise EntityError('email')
+        
+        if not User.validate_password(password):
+            raise EntityError('password')
+        
+        data = self.repo.login(email, password)
+        if data is None:
+            raise InvalidCredentials('user')
+        
+        return data
