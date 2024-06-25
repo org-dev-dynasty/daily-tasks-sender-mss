dc6ad5228f1b8bde9ff1db4066ef1a99ad40e9a7
LucaPinheiro <lucapgomes11@gmail.com>
2024-06-24 22:43:53 -0300

    refactor: Improve error handling and parameter validation in LoginController

--- src/modules/login/app/login_controller.py
+++ src/modules/login/app/login_controller.py
@@ -1,30 +1,45 @@
+from src.shared.helpers.errors.controller_errors import MissingParameters
 from .login_usecase import LoginUsecase
 from .login_viewmodel import LoginViewmodel
 from src.shared.helpers.errors.domain_errors import EntityError
-from src.shared.helpers.errors.usecase_errors import NoItemsFound
-from src.shared.helpers.external_interfaces.external_interface import IRequest
-from src.shared.helpers.external_interfaces.http_codes import OK, BadRequest, InternalServerError, NotFound
+from src.shared.helpers.errors.usecase_errors import ForbiddenAction, InvalidCredentials, NoItemsFound
+from src.shared.helpers.external_interfaces.external_interface import IRequest, IResponse
+from src.shared.helpers.external_interfaces.http_codes import OK, BadRequest, Forbidden, InternalServerError, NotFound
 
 
 class LoginController:
     def __init__(self, usecase: LoginUsecase):
         self.usecase = usecase
 
-    def handle(self, request: IRequest, email, password):
+    def __call__(self, request: IRequest) -> IResponse:
         try:
-            login = self.usecase.execute(email, password)
+            if request.data.get('email') is None:
+                raise MissingParameters('email')
 
-            print(f'usecase resp: {login}')
+            if request.data.get('password') is None:
+                raise MissingParameters('password')
 
-            viewmodel = LoginViewmodel(email, password)
+            clean_login = request.body['login'].replace(' ', '')
+            data = self.usecase(clean_login, request.data['password'])
+            login_user_viewmodel = LoginViewmodel(data)
+            response = OK(login_user_viewmodel.to_dict())
 
-            print(f'viewmodel: {viewmodel.to_dict()}')
-
-            return OK(viewmodel.to_dict())
-
+            return response
+        
         except NoItemsFound as e:
-            return NotFound(str(e))
+            return NotFound(body="User not found" if e.message == 'user' else f"User not found with parameters {e.message}")
+        
+        except ForbiddenAction as e:
+            return Forbidden(body=f"Action forbidden for {e.message}")
+        
+        except MissingParameters as e:
+            return BadRequest(body=f"Missing parameter {e.message}")
+        
         except EntityError as e:
-            return BadRequest(str(e))
+            return BadRequest(body=f"Invalid {e.message}")
+        
+        except InvalidCredentials as e:
+            return Forbidden(body=f"Invalid credentials for {e.message}")
+        
         except Exception as e:
-            return InternalServerError(str(e))
+            return InternalServerError(body=str(e))
