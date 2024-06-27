b3c24ee82d4b73472c292fb1660f4fb0504ce890
LucaPinheiro <lucapgomes11@gmail.com>
2024-06-27 02:33:32 -0300

    fixing: adding envs reply and from email to envs

--- iac/envs.ts
+++ iac/envs.ts
@@ -1,17 +1,19 @@
-import { config } from 'dotenv'
+import {config} from 'dotenv'
 import path from 'path'
 
-config({ path: path.join(__dirname, '../.env') })
+config({path: path.join(__dirname, '../.env')})
 
 const envs = {
-  STAGE: process.env.STAGE,
-  STACK_NAME: process.env.STACK_NAME,
-  REGION: process.env.REGION,
-  AWS_ACCOUNT_ID: process.env.AWS_ACCOUNT_ID,
-  SQLALCHEMY_DATABASE_URL: process.env.SQLALCHEMY_DATABASE_URL,
-  MONGODB_URL: process.env.MONGODB_URL
+    STAGE: process.env.STAGE,
+    STACK_NAME: process.env.STACK_NAME,
+    REGION: process.env.REGION,
+    AWS_ACCOUNT_ID: process.env.AWS_ACCOUNT_ID,
+    MONGODB_URL: process.env.MONGODB_URL,
+    FROM_EMAIL: process.env.FROM_EMAIL,
+    REPLY_TO_EMAIL: process.env.REPLY_TO_EMAIL
+
 }
 
-export { envs }
+export {envs}
 
 console.log(envs)
