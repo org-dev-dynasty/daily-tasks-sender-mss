7e7c8353af768e5a672ef086359d8713f6247e69
gabrielmerola <gabrielmerola12@gmail.com>
2024-06-27 02:22:46 -0300

    chore: Remove debug print statements and refactor get_all_users_presenter.py and environments.py

--- src/modules/get_all_users/app/get_all_users_presenter.py
+++ src/modules/get_all_users/app/get_all_users_presenter.py
@@ -1,18 +1,10 @@
 import logging
-import os 
-print(os.listdir(f"./"))
-print(os.listdir(f"../"))
-print(os.listdir(f"../../"))
-
+import os
 
 from src.shared.environments import Environments
-print('passou o import do env')
 from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
-print('passou o import do http lambda requests')
 from .get_all_users_usecase import GetAllUsersUsecase
-print('passou o import do usecase')
 from .get_all_users_controller import GetAllUsersController
-print('passou o import do controller')
 
 repo = Environments.get_user_repository()
 logging.info(repo)
