178ced0e7d7e4b5833f33d86b7f57d46cd399237
gabrielmerola <gabrielmerola12@gmail.com>
2024-06-27 12:26:41 -0300

    Refactor MongoDB connection in get_users_collection() for improved error handling

--- src/shared/infra/repositories/database/mongodb/user_collection.py
+++ src/shared/infra/repositories/database/mongodb/user_collection.py
@@ -1,8 +1,12 @@
 from pymongo import MongoClient
 
 def get_users_collection(conn_str: str):
-  client = MongoClient(conn_str)
-  user_db = client["daily-tasks-db"]
-  users_collection = user_db.users
-  return users_collection
-  
+  try:
+    client = MongoClient(conn_str)
+    user_db = client["daily-tasks-db"]
+    users_collection = user_db.users
+    print('MongoDB connection successful')  # Adicionar print para verificar a conexão
+    return users_collection
+  except Exception as e:
+    print(f'Erro conectando ao MongoDB: {e}')
+    raise
