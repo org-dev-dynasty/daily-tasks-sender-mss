9cd62dca4886cf9de73e1d05ef8a18b7b5a2f9bd
LucaPinheiro <lucapgomes11@gmail.com>
2024-06-27 23:50:25 -0300

    refactor: implements mock login method

--- src/shared/infra/repositories/user_repository_mock.py
+++ src/shared/infra/repositories/user_repository_mock.py
@@ -4,24 +4,28 @@ from src.shared.domain.irepositories.user_repository_interface import IUserRepos
 
 
 class UserRepositoryMock(IUserRepository):
-  def __init__(self):
-    self.users = [
-      User("Digao", "digao@gmail.com", "11999999999", "Teste12@"),
-      User("Ale", "ale@gmail.com", "11299999999", "Teste1@"),
-      User("Lucao", "lucao@gmail.com", "11399999999", "Teste13@"),
-      User("Brenheta", "brenheta@gmail.com", "11499999999", "Teste14@"),
-      User("Merola", "merola.gay@gmail.com", "11599999999", "Teste15@"),
-    ]
-    
-  def get_all_users(self) -> List[User]:
-    users = self.users
-    return users
-  
-  def create_user(self, name: str, email: str, phone: str, password: str) -> User:
-    user = User(name, email, phone, password)
-    self.users.append(user)
-    return user
-  
-  def get_user_by_id(self, user_id: int) -> User:
-    user = next((user for user in self.users if user.user_id == user_id), None)
-    return user
+    def __init__(self):
+        self.users = [
+            User("Digao", "digao@gmail.com", "11999999999", "Teste12@"),
+            User("Ale", "ale@gmail.com", "11299999999", "Teste1@"),
+            User("Lucao", "lucao@gmail.com", "11399999999", "Teste13@"),
+            User("Brenheta", "brenheta@gmail.com", "11499999999", "Teste14@"),
+            User("Merola", "merola.gay@gmail.com", "11599999999", "Teste15@"),
+        ]
+
+    def get_all_users(self) -> List[User]:
+        users = self.users
+        return users
+
+    def create_user(self, name: str, email: str, phone: str, password: str) -> User:
+        user = User(name, email, phone, password)
+        self.users.append(user)
+        return user
+
+    def login(self, email: str, password: str) -> User:
+        user = next((user for user in self.users if user.email == email and user.password == password), None)
+        return user
+
+    def get_user_by_id(self, user_id: int) -> User:
+        user = next((user for user in self.users if user.user_id == user_id), None)
+        return user
