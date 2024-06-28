8fa7c444299c4bcda06b032333a9f2a125294614
gabrielmerola <gabrielmerola12@gmail.com>
2024-06-28 11:59:30 -0300

    refactor: Update User repository mock to include additional user information

--- src/shared/infra/repositories/user_repository_mock.py
+++ src/shared/infra/repositories/user_repository_mock.py
@@ -6,19 +6,19 @@ from src.shared.domain.irepositories.user_repository_interface import IUserRepos
 class UserRepositoryMock(IUserRepository):
     def __init__(self):
         self.users = [
-            User("Digao", "digao@gmail.com", "11999999999", "Teste12@"),
-            User("Ale", "ale@gmail.com", "11299999999", "Teste1@"),
-            User("Lucao", "lucao@gmail.com", "11399999999", "Teste13@"),
-            User("Brenheta", "brenheta@gmail.com", "11499999999", "Teste14@"),
-            User("Merola", "merola.gay@gmail.com", "11599999999", "Teste15@"),
+            User("Digao", "digao@gmail.com", "11999999999", "Teste12@", True, False),
+            User("Ale", "ale@gmail.com", "11299999999", "Teste1@", True, False),
+            User("Lucao", "lucao@gmail.com", "11399999999", "Teste13@", False, False),
+            User("Brenheta", "brenheta@gmail.com", "11499999999", "Teste14@", True, False),
+            User("Merola", "merola.gay@gmail.com", "11599999999", "Teste15@", True, True, '1'),
         ]
 
     def get_all_users(self) -> List[User]:
         users = self.users
         return users
 
-    def create_user(self, name: str, email: str, phone: str, password: str) -> User:
-        user = User(name, email, phone, password)
+    def create_user(self, name: str, email: str, phone: str, password: str, accepted_terms: bool, accepted_notifications_email: bool) -> User:
+        user = User(name, email, phone, password, accepted_terms, accepted_notifications_email)
         self.users.append(user)
         return user
 
