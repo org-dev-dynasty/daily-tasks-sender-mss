9bf5fa415e493ffa1ce4c730b1bc5ce4bf009d3f
LucaPinheiro <lucapgomes11@gmail.com>
2024-06-27 23:49:59 -0300

    refactor: adding login method

--- src/shared/domain/irepositories/user_repository_interface.py
+++ src/shared/domain/irepositories/user_repository_interface.py
@@ -12,6 +12,9 @@ class IUserRepository(ABC):
   @abstractmethod
   def create_user(self, name: str, email: str, phone: str, password: str) -> User:
     pass
+
+  def login_user(self, login: str, password: str) -> dict:
+    pass
   
   # @abstractmethod
   # def get_user_by_id(self, user_id: int) -> User:
