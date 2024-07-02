7336f9a2934ceaa12ef2e6f92712dbff86cf5fc2
gabrielmerola <gabrielmerola12@gmail.com>
2024-07-02 18:42:53 -0300

    refactor: Update User.parse_object method to handle missing user_id field

--- src/shared/domain/entities/user.py
+++ src/shared/domain/entities/user.py
@@ -109,15 +109,14 @@ class User(abc.ABC):
     @staticmethod
     def parse_object(user: dict) -> 'User':
         return User(
-            user_id=user.get("user_id"),
             email=user['email'],
             name=user['name'].title(),
+            phone=user['phone'] if user.get('phone') is not None else None,
             password=user['password'] if user.get('password') is not None else None,
             accepted_terms=user['accepted_terms'] if user.get('accepted_terms') is not None else None,
-            phone=user['phone'] if user.get('phone') is not None else None,
-            accepted_notifications_email=user['accepted_notifications_email'] if user.get('accepted_notifications_email') is not None else None
+            accepted_notifications_email=user['accepted_notifications_email'] if user.get('accepted_notifications_email') is not None else None,
+            user_id=user.get("user_id")
         )
-
 
     def to_dict(self) -> dict:
         return {
