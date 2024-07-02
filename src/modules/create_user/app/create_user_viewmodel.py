4f8267423e52abac353ade7b1d2918509c905ff9
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-02 14:00:29 -0300

    refactor: Improve UserViewmodel and CreateUserViewmodel classes

---
+++ src/modules/create_user/app/create_user_viewmodel.py
@@ -0,0 +1,31 @@
+from src.shared.domain.entities.user import User
+
+
+class UserViewmodel:
+    name: str
+    email: str
+
+    def __init__(self, user: User):
+        self.user_id = user.user_id
+        self.name = user.name
+        self.email = user.email
+
+    def to_dict(self):
+        return {
+            'user_id': self.user_id,
+            'name': self.name,
+            'email': self.email,
+        }
+
+
+class CreateUserViewmodel:
+    user: UserViewmodel
+
+    def __init__(self, user: User):
+        self.user = UserViewmodel(user)
+
+    def to_dict(self):
+        return {
+            'user': self.user.to_dict(),
+            'message': 'the user was created'
+        }
