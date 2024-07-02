3a709d9bf3f8b9f2c88d1465cc29265ec7d544a2
gabrielmerola <gabrielmerola12@gmail.com>
2024-07-02 18:43:03 -0300

    feat: Add test for CreateUserController

---
+++ tests/modules/create_user/app/test_create_user_controller.py
@@ -0,0 +1,26 @@
+from src.shared.infra.repositories.user_repository_mock import UserRepositoryMock
+from src.modules.create_user.app.create_user_controller import CreateUserController
+from src.modules.create_user.app.create_user_usecase import CreateUserUsecase
+from src.shared.helpers.external_interfaces.http_models import HttpRequest
+
+class Test_CreateUserController:
+
+    def test_create_user_controller(self):
+        repo = UserRepositoryMock()
+        usecase = CreateUserUsecase(repo)
+        controller = CreateUserController(usecase)
+        request = HttpRequest(body={
+            'name': 'John Doe',
+            'email': 'john@gmail.com',
+            'password': 'Teste@01',
+            'phone': '11987655142',
+            'accepted_terms': True,
+            'accepted_notifications_email': True
+        })
+
+        response = controller(request)
+
+        assert response.status_code == 201
+        assert response.body['user']['name'] == 'John Doe'
+        assert response.body['user']['email'] == 'john@gmail.com'
+        assert response.body['message'] == 'the user was created'
