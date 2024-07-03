13f931d14c5ddf554e0ae4edcfa6fa5e458baceb
gabrielmerola <gabrielmerola12@gmail.com>
2024-07-03 14:19:49 -0300

    feat: Add logging statements to create_task_presenter module

--- src/modules/create_task/app/create_task_presenter.py
+++ src/modules/create_task/app/create_task_presenter.py
@@ -1,11 +1,16 @@
+import logging
+
 from src.shared.environments import Environments
 from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
 from .create_task_usecase import CreateTaskUsecase
 from .create_task_controller import CreateTaskController
 
 repo = Environments.get_task_repo()()
+logging.info(repo)
 usecase = CreateTaskUsecase(repo)
+logging.info(usecase)
 controller = CreateTaskController(usecase)
+logging.info(controller)
 
 def lambda_handler(event, context):
     httpRequest = LambdaHttpRequest(event)
