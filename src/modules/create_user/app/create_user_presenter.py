c6f8c4e3b0472b24aced87a3ae567b01f84a6002
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-03 17:16:24 -0300

    chore: Refactor create_user_presenter.py for improved readability and logging

--- src/modules/create_user/app/create_user_presenter.py
+++ src/modules/create_user/app/create_user_presenter.py
@@ -1,15 +1,21 @@
 import logging
-
+print("ENTRANDO NO PRESENTER")
 from src.shared.environments import Environments
 from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
 from .create_user_usecase import CreateUserUsecase
 from .create_user_controller import CreateUserController
 
+print("IMPORTOU TUDO e entrando no REPO DE USER")
 repo = Environments.get_user_repo()()
+print("REPO DE USER INSTANCIADO")
 logging.info(repo)
+print("CHAMANDO O USECASE DE CREATE USER")
 usecase = CreateUserUsecase(repo)
+print('USECASE DE CREATE USER INSTANCIADO')
 logging.info(usecase)
+print("CHAMANDO O CONTROLLER DE CREATE USER")
 controller = CreateUserController(usecase)
+print("CONTROLLER DE CREATE USER INSTANCIADO")
 logging.info(controller)
 
 
