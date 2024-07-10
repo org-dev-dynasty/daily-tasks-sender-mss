5ce9e33a7ccf2deee28caadbdc2f536df004458b
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-07-10 11:50:26 -0300

    refactor: changed the name of db on getting users collection

--- src/shared/infra/repositories/database/mongodb/user_collection.py
+++ src/shared/infra/repositories/database/mongodb/user_collection.py
@@ -3,9 +3,9 @@ from pymongo import MongoClient
 def get_users_collection(conn_str: str):
   try:
     client = MongoClient(conn_str)
-    user_db = client["daily-tasks-db"]
-    users_collection = user_db.users
-    print('MongoDB connection successful')  # Adicionar print para verificar a conexão
+    daily_tasks_db = client["daily-tasks-db"]
+    users_collection = daily_tasks_db.users
+    print('MongoDB connection successful')
     return users_collection
   except Exception as e:
     print(f'Erro conectando ao MongoDB: {e}')
