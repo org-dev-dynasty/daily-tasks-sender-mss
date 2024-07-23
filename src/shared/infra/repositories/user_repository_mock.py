9c687732919d6506d231801ad99471cc5a320b31
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-07-23 18:55:25 -0300

    refactor: fixed tests

--- src/shared/infra/repositories/user_repository_mock.py
+++ src/shared/infra/repositories/user_repository_mock.py
@@ -33,3 +33,10 @@ class UserRepositoryMock(IUserRepository):
     def confirm_user(self, email: str, verification_code: str) -> User:
         user = next((user for user in self.users if user.email == email), None)
         return user
+    
+    def create_user_oauth(self, user) -> dict:
+        if self.get_user_by_email(user.email):
+            raise DuplicatedItem("email")
+        user.user_id = str(len(self.users) + 1)
+        self.users.append(user)
+        return {"message": "User created successfully"}
