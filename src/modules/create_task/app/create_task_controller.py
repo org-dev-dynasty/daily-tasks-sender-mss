a902f39aead4d0f568576cc7da8aed29d795d20e
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-23 23:57:08 -0300

    feat: Remove unnecessary print statements in CreateTaskController

--- src/modules/create_task/app/create_task_controller.py
+++ src/modules/create_task/app/create_task_controller.py
@@ -25,17 +25,13 @@ class CreateTaskController:
 
             
             if request.data.get('task_description') is None:
-                print("FUNFOU CACETE")
                 task_description = None
             else:
-                print("PROVAVELMENTE DEU MERDA!!!!!!!!!!!!")
                 task_description = request.data.get('task_description')
                 
             if request.data.get('task_local') is None:
-                print('funfou pra caralho!')
                 task_local = None
             else:
-                print("deu merda..")
                 task_local = request.data.get('task_local')
             
             task_dict = {
