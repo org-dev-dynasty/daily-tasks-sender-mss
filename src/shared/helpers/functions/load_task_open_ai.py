0d8d575c5338da1a06e4e1ac04b38698e5bf6849
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-23 23:04:36 -0300

    feat: Refactor OpenAI task loading function

--- src/shared/helpers/functions/load_task_open_ai.py
+++ src/shared/helpers/functions/load_task_open_ai.py
@@ -1,31 +1,31 @@
 import openai
 from src.shared.environments import Environments
 
-def load_openai(system_prompt, user_prompt):
-    api_key = Environments.get_envs().open_ai_api_key
-    if not api_key:
-        raise ValueError("API Key for OpenAI not found in environment variables.")
 
-    openai.api_key = api_key
-    model = Environments.get_envs().open_ai_model
-    prompt = Environments.get_envs().prompt
-
+def load_openai(user_prompt):
+    if not user_prompt:
+        raise ValueError("User prompt is required to generate response.")
+    str(user_prompt)
     try:
+        api_key = Environments.get_envs().open_ai_api_key
+        if not api_key:
+            raise ValueError(
+                "API Key for OpenAI not found in environment variables.")
+
+        openai.api_key = api_key
+        model = Environments.get_envs().open_ai_model
+        system_prompt = Environments.get_envs().prompt
+
         response = openai.ChatCompletion.create(
             model=model,
-            system_prompt=prompt,
             messages=[
                 {"role": "system", "content": system_prompt},
                 {"role": "user", "content": user_prompt}
             ],
-            max_tokens=200,
+            max_tokens=4096,
         )
         response_text = response['choices'][0]['message']['content']
-        print(response_text)
+        return response_text
 
     except Exception as e:
         print(f"An error occurred: {e}")
-
-system_prompt = "Your system prompt here"
-user_prompt = "Your user prompt here"
-load_openai(system_prompt, user_prompt)
