a123236d5767698d03e51413036eb58b6b4b2a9d
gabrielmerola <gabrielmerola12@gmail.com>
2024-07-25 04:26:43 -0300

    refactor: Update GetAllCategoriesViewmodel to include category_id parameter in constructor

--- src/modules/get_all_categories/app/get_all_categories_viewmodel.py
+++ src/modules/get_all_categories/app/get_all_categories_viewmodel.py
@@ -1,58 +1,50 @@
-from typing import Optional
-
+from typing import Optional, List, Dict
 
 class CategoryViewmodel:
-  category_id: str
-  category_name: Optional[str]
-  category_primary_color: str
-  category_secondary_color: str
-  
-  def __init__(
-    self, 
-    category_id: str,
-    category_name: Optional[str],
-    category_primary_color: str,
+    category_id: str
+    category_name: Optional[str]
+    category_primary_color: str
     category_secondary_color: str
-  ):
-    self.category_id = category_id
-    self.category_primary_color = category_primary_color
-    self.category_secondary_color = category_secondary_color
-    if category_name:
-      self.category_name = category_name
-    else:
-      self.category_name = None
     
-  def to_dict(self) -> dict:
-    return {
-      "category_id": self.category_id,
-      "category_name": self.category_name,
-      "category_primary_color": self.category_primary_color,
-      "category_secondary_color": self.category_secondary_color
-    }
+    def __init__(
+        self, 
+        category_id: str,
+        category_name: Optional[str],
+        category_primary_color: str,
+        category_secondary_color: str
+    ):
+        self.category_id = category_id
+        self.category_name = category_name
+        self.category_primary_color = category_primary_color
+        self.category_secondary_color = category_secondary_color
+    
+    def to_dict(self) -> dict:
+        return {
+            "category_id": self.category_id,
+            "category_name": self.category_name,
+            "category_primary_color": self.category_primary_color,
+            "category_secondary_color": self.category_secondary_color
+        }
     
 class GetAllCategoriesViewmodel:
-  categories_viewmodel_list: list
-  
-  def __init__(self, categories: list) -> None:
-    categories_list = []
-    for category in categories:
-      category_viewmodel = CategoryViewmodel(
-        category.category_id,
-        category.category_name,
-        category.category_primary_color,
-        category.category_secondary_color
-      )
-      categories_list.append(category_viewmodel)
+    categories_viewmodel_list: List[CategoryViewmodel]
     
-    self.categories_viewmodel_list = categories_list
+    def __init__(self, categories: List[Dict]) -> None:
+        categories_list = []
+        for category in categories:
+            category_viewmodel = CategoryViewmodel(
+                category['_id'],
+                category.get('category_name'),
+                category['category_primary_color'],
+                category['category_secondary_color']
+            )
+            categories_list.append(category_viewmodel)
+        
+        self.categories_viewmodel_list = categories_list
     
-  def to_dict(self) -> dict:
-    categories_list = []
-    for category_viewmodel in self.categories_viewmodel_list:
-      category_viewmodel_to_dict = category_viewmodel.to_dict()
-      categories_list.append(category_viewmodel_to_dict)
-    
-    return {
-      "categories": categories_list,
-      "message": "Todas as categorias retornadas com sucesso"
-    }
+    def to_dict(self) -> dict:
+        categories_list = [category_viewmodel.to_dict() for category_viewmodel in self.categories_viewmodel_list]
+        return {
+            "categories": categories_list,
+            "message": "Todas as categorias retornadas com sucesso"
+        }
