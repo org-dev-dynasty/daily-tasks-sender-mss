946f029f57b9505e82a8e5cef8a7039d0c24dbff
gabrielmerola <gabrielmerola12@gmail.com>
2024-07-25 04:00:07 -0300

    refactor: Include category_id parameter in update_task method of TaskRepositoryInterface

--- src/shared/domain/irepositories/task_repository_interface.py
+++ src/shared/domain/irepositories/task_repository_interface.py
@@ -18,7 +18,7 @@ class ITaskRepository(ABC):
   def get_task_by_id(self, task_id: str) -> Task:
     pass
 
-  def update_task(self, task_id: str, task_name: Optional[str], task_date: Optional[date], task_hour: Optional[time], task_description: Optional[str], task_local: Optional[str], task_status: Optional[str]) -> Task:
+  def update_task(self, task_id: str, category_id: str, task_name: Optional[str], task_date: Optional[date], task_hour: Optional[time], task_description: Optional[str], task_local: Optional[str], task_status: Optional[str]) -> Task:
     pass
   
   def delete_task_by_id(self, task_id: str) -> None:
