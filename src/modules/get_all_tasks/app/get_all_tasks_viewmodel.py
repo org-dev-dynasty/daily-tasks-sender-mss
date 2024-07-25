55d95663ff805bdfe538e4468c65b477f04e26b6
gabrielmerola <gabrielmerola12@gmail.com>
2024-07-24 23:35:35 -0300

    Refactor task date key generation in GetAllTasksViewmodel

--- src/modules/get_all_tasks/app/get_all_tasks_viewmodel.py
+++ src/modules/get_all_tasks/app/get_all_tasks_viewmodel.py
@@ -58,14 +58,13 @@ class GetAllTasksViewmodel:
     
     def to_dict(self) -> dict:
         tasks_by_date = defaultdict(list)
-        Task.validate_date
         current_day = date.today().isoformat()
         
         for task_viewmodel in self.tasks_viewmodel_list:
             task_date = task_viewmodel.task_date
             if task_date == current_day:
                 date_key = "Hoje"
-            elif task_date == current_day + timedelta(days=1):
+            elif task_date == datetime.now().date() + timedelta(days=1):
                 date_key = "Amanhã"
             else:
                 date_key = task_date
