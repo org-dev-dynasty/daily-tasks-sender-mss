369572dfa11fd412b7415702ffd525b679e1d7fc
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-07-29 17:53:47 -0300

    refactor: Update speech_to_text method in IAudioRepository to accept file_name parameter

--- src/shared/domain/irepositories/audio_repository_interface.py
+++ src/shared/domain/irepositories/audio_repository_interface.py
@@ -2,5 +2,5 @@ from abc import ABC, abstractmethod
 
 class IAudioRepository(ABC):
   @abstractmethod
-  def speech_to_text(self, audio_path) -> str:
+  def speech_to_text(self, audio_path, file_name) -> str:
     pass
