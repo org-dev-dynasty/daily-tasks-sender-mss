b2aba19f509e4930a41a95e4cddc2f9887699a06
gabrielmerola <gabrielmerola12@gmail.com>
2024-07-29 13:48:36 -0300

    feat: Update GetAllInactivesTasksController to return success message

--- src/modules/get_all_inactives_tasks/app/get_all_inactives_tasks_controller.py
+++ src/modules/get_all_inactives_tasks/app/get_all_inactives_tasks_controller.py
@@ -22,9 +22,9 @@ class GetAllInactivesTasksController:
                 user_id = UserAPIGatewayDTO.from_api_gateway(request.data.get('requester_user')).to_dict().get('user_id')
             
             tasks = self.usecase.execute(user_id=user_id)
-            viewmodel = GetAllInactivesTasksViewmodel(tasks)
+            viewmodel = GetAllInactivesTasksViewmodel('Tasks encontradas com sucesso!')
 
-            return OK(viewmodel.to_dict())
+            return OK(viewmodel.to_dict(tasks=tasks))
             # return OK(tasks)
         except NoItemsFound as e:
             return NotFound(str(e))
