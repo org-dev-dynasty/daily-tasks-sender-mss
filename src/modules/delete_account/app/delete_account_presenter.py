a9f72c19c8db31568372756338cc969169ab4f0d
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-07-29 12:45:37 -0300

    feat: Add delete account presenter

---
+++ src/modules/delete_account/app/delete_account_presenter.py
@@ -0,0 +1,15 @@
+from src.shared.environments import Environments
+from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
+from .delete_account_controller import DeleteAccountController
+from .delete_account_usecase import DeleteAccountUsecase
+
+repo = Environments.get_task_repo()
+usecase = DeleteAccountUsecase(repo)
+controller = DeleteAccountController(usecase)
+
+def lambda_handler(event, context):
+  httpRequest = LambdaHttpRequest(event)
+  httpRequest.data['requester_user'] = event.get('requestContext', {}).get('authorizer', {}).get('claims', None)
+  response = controller(httpRequest)
+  httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
+  return httpResponse.to_dict()
