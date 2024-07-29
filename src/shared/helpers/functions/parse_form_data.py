491f00685e6e79469082758b871f9c5002fd554c
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-07-29 16:14:38 -0300

    refactor: added lib io to transform body bytes to stream readable to multipart parser

--- src/shared/helpers/functions/parse_form_data.py
+++ src/shared/helpers/functions/parse_form_data.py
@@ -1,5 +1,6 @@
 from multipart import parse_options_header, MultipartParser
 import base64
+import io
 
 from src.shared.helpers.external_interfaces.external_interface import IRequest
 
@@ -11,7 +12,9 @@ def formdata_parser(event):
     boundary = options['boundary'].encode('utf-8')
     body = base64.b64decode(event["body"]) if event["isBase64Encoded"] else event["body"].encode('utf-8')
     
-    parser = MultipartParser(body, boundary)
+    body_stream = io.BytesIO(body)
+    
+    parser = MultipartParser(body_stream, boundary)
     
     return parser
     
