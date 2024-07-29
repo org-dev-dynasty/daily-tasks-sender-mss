37a7829eb2f634e905f9ab61a26f723074d3c0f9
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-07-29 12:19:17 -0300

    feat: Add OpenAI audio repository implementation for speech-to-text functionality

---
+++ src/shared/infra/repositories/audio_repository_openai.py
@@ -0,0 +1,22 @@
+import openai
+from src.shared.domain.irepositories.audio_repository_interface import IAudioRepository
+from src.shared.environments import Environments
+
+class AudioRepositoryOpenAI(IAudioRepository):
+  def __init__(self):
+    self.api_key = Environments.get_envs().open_ai_api_key
+    openai.api_key = self.api_key
+
+  def speech_to_text(self, audio_buffer) -> str:
+    try:
+      response = openai.Audio.transcribe(
+        model="whisper-1",
+        file=audio_buffer,
+        language="pt"
+      )
+      return response['text']
+    
+    except Exception as e:
+      print(f'ERROR TRANSCRIBING AUDIO: ')
+      print(str(e))
+      raise ValueError("An error occurred while transcribing audio")
