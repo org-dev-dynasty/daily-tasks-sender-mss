0d736a7748d440bed126739e5f4c47372d4208c7
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-07-29 14:37:36 -0300

    refactor: Update transcribe_audio_presenter.py to use formdata_parser for parsing form data

--- src/modules/transcribe_audio/app/transcribe_audio_presenter.py
+++ src/modules/transcribe_audio/app/transcribe_audio_presenter.py
@@ -1,5 +1,6 @@
 from src.shared.environments import Environments
 from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
+from src.shared.helpers.functions.parse_form_data import formdata_parser
 from src.shared.infra.repositories.audio_repository_openai import AudioRepositoryOpenAI
 from .transcribe_audio_controller import TranscribeAudioController
 from .transcribe_audio_usecase import TranscribeAudioUsecase
@@ -10,7 +11,9 @@ controller = TranscribeAudioController(usecase)
 
 def lambda_handler(event, context):
   print(event)
+  parser = formdata_parser(event)
   httpRequest = LambdaHttpRequest(event)
+  httpRequest.data['formdata_parser'] = parser
   # httpRequest.data['requester_user'] = event.get('requestContext', {}).get('authorizer', {}).get('claims', None)
   response = controller(httpRequest)
   httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
