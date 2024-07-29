452bd5237bd1f547ec0b0ee848c33f0543cf33b9
gabrielmerola <gabrielmerola12@gmail.com>
2024-07-29 13:19:46 -0300

    feat: Add GetAllInactivesTasksUsecase to handle retrieving inactive tasks

---
+++ src/modules/get_all_inactives_tasks/app/get_all_inactives_tasks_usecase.py
@@ -0,0 +1,16 @@
+from src.shared.domain.irepositories.task_repository_interface import ITaskRepository
+from src.shared.helpers.errors.usecase_errors import NoItemsFound
+
+
+class GetAllInactivesTasksUsecase:
+    def __init__(self, repo: ITaskRepository):
+        self.repo = repo
+    
+    def execute(self, user_id: str):
+
+        tasks = self.repo.get_all_inactives_tasks(user_id=user_id)
+        
+        if len(tasks) == 0:
+            raise NoItemsFound("tasks")
+
+        return tasks
