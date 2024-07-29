041a084ba1c7b0c90c65d7c4d2a80ca01c695647
gabrielmerola <gabrielmerola12@gmail.com>
2024-07-29 13:19:37 -0300

    refactor: Add GetAllInactivesTasksController to handle retrieving inactive tasks

---
+++ src/modules/get_all_inactives_tasks/app/get_all_inactives_tasks_controller.py
@@ -0,0 +1,34 @@
+from src.modules.get_all_inactives_tasks.app.get_all_inactives_tasks_usecase import GetAllInactivesTasksUsecase
+from src.modules.get_all_inactives_tasks.app.get_all_inactives_tasks_viewmodel import GetAllInactivesTasksViewmodel
+from src.shared.domain.enums.stage_enum import STAGE
+from src.shared.environments import Environments
+from src.shared.helpers.errors.controller_errors import MissingParameters
+from src.shared.helpers.errors.domain_errors import EntityError
+from src.shared.helpers.errors.usecase_errors import NoItemsFound
+from src.shared.helpers.external_interfaces.external_interface import IRequest
+from src.shared.helpers.external_interfaces.http_codes import OK, BadRequest, InternalServerError, NotFound
+from src.shared.infra.dto.user_api_gateway_dto import UserAPIGatewayDTO
+
+
+class GetAllInactivesTasksController:
+    def __init__(self, usecase: GetAllInactivesTasksUsecase):
+        self.usecase = usecase
+    
+    def __call__(self, request: IRequest):
+        try:
+            if Environments.get_envs().stage is not STAGE.TEST:
+                if request.data.get('requester_user') is None:
+                    raise MissingParameters('requester_user')
+                user_id = UserAPIGatewayDTO.from_api_gateway(request.data.get('requester_user')).to_dict().get('user_id')
+            
+            tasks = self.usecase.execute(user_id=user_id)
+            viewmodel = GetAllInactivesTasksViewmodel(tasks)
+
+            return OK(viewmodel.to_dict())
+            # return OK(tasks)
+        except NoItemsFound as e:
+            return NotFound(str(e))
+        except EntityError as e:
+            return BadRequest(str(e))
+        except Exception as e:
+            return InternalServerError(str(e))
