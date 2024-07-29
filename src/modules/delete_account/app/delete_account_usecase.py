74b932fcdcc8ee2485f34b7c107bac66e3f4dedd
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-07-29 12:45:58 -0300

    feat: Add delete account use case

---
+++ src/modules/delete_account/app/delete_account_usecase.py
@@ -0,0 +1,11 @@
+from src.shared.domain.irepositories.user_repository_interface import IUserRepository
+
+
+class DeleteAccountUsecase:
+  def __init__(self, repo: IUserRepository):
+    self.repo = repo
+    
+  def __call__(self, user_id):
+    self.repo.delete_user(user_id)
+    
+    return 'User deleted'
