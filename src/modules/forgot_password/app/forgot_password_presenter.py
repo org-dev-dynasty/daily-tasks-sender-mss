22a697995a92d827b7f0c51a250c5c508aedd263
gabrielmerola <gabrielmerola12@gmail.com>
2024-07-29 19:43:01 -0300

    refactor: Update forgot_password_presenter.py to use LambdaHttpRequest and LambdaHttpResponse
    This change updates the `forgot_password_presenter.py` file to import the `LambdaHttpRequest` and `LambdaHttpResponse` classes from the `src.shared.helpers.external_interfaces.http_lambda_requests` module. It also initializes the `repo`, `usecase`, and `controller` objects and adds a `lambda_handler` function that handles the Lambda event and returns a Lambda HTTP response.

--- src/modules/forgot_password/app/forgot_password_presenter.py
+++ src/modules/forgot_password/app/forgot_password_presenter.py
@@ -1,1 +1,15 @@
-// Conteúdo do arquivo...
+from src.shared.environments import Environments
+from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
+from src.modules.forgot_password.app.forgot_password_controller import ForgotPasswordController
+from src.modules.forgot_password.app.forgot_password_usecase import ForgotPasswordUsecase
+
+
+repo = Environments.get_user_repo()
+usecase = ForgotPasswordUsecase(repo)
+controller = ForgotPasswordController(usecase)
+
+def lambda_handler(event, context):
+    httpRequest = LambdaHttpRequest(event)
+    response = controller(httpRequest)
+    httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
+    return httpResponse.to_dict()
