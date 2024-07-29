c61aa16ae6f741a7b4ba7a96784fc3e961862f75
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-07-29 17:53:43 -0300

    refactor: Update TranscribeAudioUsecase to accept file name parameter
    The TranscribeAudioUsecase was updated to accept a file name parameter in addition to the audio buffer. This change allows for more flexibility in transcribing audio files and aligns with recent updates in related classes and methods.

--- src/modules/transcribe_audio/app/transcribe_audio_usecase.py
+++ src/modules/transcribe_audio/app/transcribe_audio_usecase.py
@@ -5,8 +5,8 @@ class TranscribeAudioUsecase:
   def __init__(self, repo: IAudioRepository):
     self.repo = repo
     
-  def __call__(self, audio_buffer):
-    audio_transcribed = self.repo.speech_to_text(audio_buffer)
+  def __call__(self, audio_buffer, file_name):
+    audio_transcribed = self.repo.speech_to_text(audio_buffer, file_name)
     
     return audio_transcribed
     
