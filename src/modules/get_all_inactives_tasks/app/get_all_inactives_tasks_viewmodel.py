d247dd3e14b288524057e468312055e669ff22eb
gabrielmerola <gabrielmerola12@gmail.com>
2024-07-29 13:48:41 -0300

    refactor: Update GetAllInactivesTasksViewmodel to include tasks in the response

--- src/modules/get_all_inactives_tasks/app/get_all_inactives_tasks_viewmodel.py
+++ src/modules/get_all_inactives_tasks/app/get_all_inactives_tasks_viewmodel.py
@@ -1,10 +1,16 @@
+from src.shared.domain.entities.task import Task
+
+
 class GetAllInactivesTasksViewmodel:
     message: str
+    tasks: list[Task]
 
     def __init__(self, message: str):
         self.message = message
 
-    def to_dict(self):
+
+    def to_dict(self, tasks: list[Task]):
         return {
-            "message": self.message
+            "message": self.message,
+            "tasks": [task.to_dict() for task in tasks]
         }
