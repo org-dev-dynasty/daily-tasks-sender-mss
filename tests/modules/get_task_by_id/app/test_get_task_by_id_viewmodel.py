c09912698c7e2da016c2108a5639ecccc1fda716
gabrielmerola <gabrielmerola12@gmail.com>
2024-07-29 22:37:05 -0300

    refactor: Update GetTaskByIdViewmodel to use task_id as string

--- tests/modules/get_task_by_id/app/test_get_task_by_id_viewmodel.py
+++ tests/modules/get_task_by_id/app/test_get_task_by_id_viewmodel.py
@@ -4,16 +4,22 @@ from src.modules.get_task_by_id.app.get_task_by_id_viewmodel import GetTaskByIdV
 
 class Test_GetProductViewmodel:
 
-    def test_get_product_viewmodel(self):
+    def test_get_task_by_id_viewmodel(self):
         repo = TaskRepositoryMock()
 
         task = repo.tasks[0]
 
         task_viewmodel = GetTaskByIdViewmodel(task).to_dict()
 
-        excepted = {
+        expected = {
             "task": {
                 "task_id": "1",
+                "category": {
+                    "category_id": "1",
+                    "category_name": "Categoria 1",
+                    "category_primary_color": "#123456",
+                    "category_secondary_color": "#654321"
+                },
                 "task_name": 'TaskUm',
                 "task_date": "2021-12-12",
                 "task_hour": "12:00:00",
@@ -21,7 +27,7 @@ class Test_GetProductViewmodel:
                 "task_local": "Local 1",
                 "task_status": "ACTIVE"
             },
-            'message': 'the task was retrieved'
+            "message": "Task retornada com sucesso"
         }
 
-        assert task_viewmodel == excepted
+        assert task_viewmodel == expected
