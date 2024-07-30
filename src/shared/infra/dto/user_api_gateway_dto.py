cc41b0ea7078b636ba658ea9d9ec949293cfca7c
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-30 00:12:47 -0300

    refactor: Update DeleteAccountController to use email instead of user_id

--- src/shared/infra/dto/user_api_gateway_dto.py
+++ src/shared/infra/dto/user_api_gateway_dto.py
@@ -1,16 +1,20 @@
 class UserAPIGatewayDTO:
-  user_id: str
-  
-  def __init__(self, user_id: str) -> None:
-    self.user_id = user_id
-    
-  @staticmethod
-  def from_api_gateway(data: dict) -> "UserAPIGatewayDTO":
-    return UserAPIGatewayDTO(
-      user_id=data['sub']
-    )
-    
-  def to_dict(self) -> dict:
-    return {
-      'user_id': self.user_id
-    }
+    user_id: str
+    username: str
+
+    def __init__(self, user_id: str, username: str) -> None:
+        self.user_id = user_id
+        self.username = username
+
+    @staticmethod
+    def from_api_gateway(data: dict) -> "UserAPIGatewayDTO":
+        return UserAPIGatewayDTO(
+            user_id=data['sub'],
+            username=data['cognito:username']
+        )
+
+    def to_dict(self) -> dict:
+        return {
+            'user_id': self.user_id,
+            'username': self.username
+        }
