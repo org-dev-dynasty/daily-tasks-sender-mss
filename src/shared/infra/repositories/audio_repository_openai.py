52850649ee6733e7a7c6134a4f2ab26001efcc31
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-07-29 23:06:10 -0300

    refactor: Update AudioRepositoryOpenAI to use audio buffer instead of audio file
    This change improves efficiency and eliminates the need for unnecessary file operations in the AudioRepositoryOpenAI class by updating it to use an audio buffer instead of an audio file.

--- src/shared/infra/repositories/audio_repository_openai.py
+++ src/shared/infra/repositories/audio_repository_openai.py
@@ -2,6 +2,7 @@ import openai
 import os
 from src.shared.domain.irepositories.audio_repository_interface import IAudioRepository
 from src.shared.environments import Environments
+import requests
 
 class AudioRepositoryOpenAI(IAudioRepository):
   def __init__(self):
@@ -12,11 +13,20 @@ class AudioRepositoryOpenAI(IAudioRepository):
     try:      
       audio_file = open(path, "rb")
 
-      response = openai.Audio.transcribe(
-            model="whisper-1",
-            file=audio_file,
-            language="pt"
-        )
+      request_files = {
+        "file": audio_file,
+        "model": (None, "whisper-1"),
+      }
+      
+      
+      
+      response = requests.post('https://api.openai.com/v1/audio/transcriptions', files=request_files, headers={
+        'Content-Type': 'multipart/form-data',
+        'Authorization': f'Bearer {Environments.get_envs().open_ai_api_key}'
+      })
+      
+      print('response: ')
+      print(response)
       
       # delete the file
       os.remove(path)
